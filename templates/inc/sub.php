f0e2e9e3992570cee33d6a9b43b46eb72597c90d
Stefan Imhoff <stefan@imhoff.name>
2008-03-14 17:07:18 -0500

    MediaTemple-Logo gelöscht, Webfaction-Logo eingebunden.

--- templates/inc/sub.php
+++ templates/inc/sub.php
@@ -5,7 +5,7 @@
           <a href="{link_cc}"><img src="{path='site_index'}css/images/logo-cc.png" width="36" height="36" alt="CreativeCommons" title="Inhalte unter CreativeCommons-Lizenz veröffentlicht" /></a>
           <a href="{link_ee}"><img src="{path="site_index"}css/images/logo-ee.png" width="55" height="36" alt="ExpressionEngine" title="Angetrieben mit der Publishing-Engine ExpressionEngine" /></a>
           <a href="{link_apple}"><img src="{path='site_index'}css/images/logo-apple.png" width="29" height="36" alt="Apple" title="Made on a Mac" /></a>
-          <a href="{link_mt}"><img src="{path='site_index'}css/images/logo-mt.png" width="91" height="36" alt="Media Temple" title="Gehosted von Media Temple"/></a>
+          <a href="{link_webfaction}"><img src="{path='site_index'}css/images/logo-webfaction.png" width="48" height="36" alt="Webfaction" title="Gehosted von Webfaction"/></a>
         </div>
         <p>© 2006-{current_time format="%Y"}, {owner}. Die Inhalte sind unter einer <a href="{link_cc}">CreativeCommons-Lizenz</a> veröffentlicht.</p>
         <p>Angetrieben mit <a href="{link_ee}">ExpressionEngine {app_version}</a> und gehostet von <a href="{link_mt}">(mt) Media Temple</a>. <a href="{path='information/impressum'}" rel="nofollow">Impressum</a>, <a href="{path='information/datenschutz'}" rel="nofollow">Datenschutz</a></p>
