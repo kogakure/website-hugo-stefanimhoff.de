4b5a379e25a86649aaa03129c3eb6ac447d5abc2
Stefan Imhoff <stefan@imhoff.name>
2009-01-17 09:35:49 +0100

    Feedburner-Pfad angepasst

--- templates/inc/js.php
+++ templates/inc/js.php
@@ -1,6 +1,6 @@
     <script type="text/javascript" charset="{charset}" src="{path='site_index'}js/mootools.js"></script>
     <script type="text/javascript" charset="{charset}" src="{path='site_index'}js/scripts.js"></script>
     <script src="http://mint.stefanimhoff.de/?js" type="text/javascript"></script>
-    <link rel="alternate" type="application/atom+xml" title="Notizen" href="http://feeds.feedburner.com/stefanimhoff/notizen/" />
-    <link rel="alternate" type="application/atom+xml" title="Randbemerkungen" href="http://feeds.feedburner.com/stefanimhoff/randbemerkungen/" />
-    <link rel="alternate" type="application/atom+xml" title="Projekte" href="http://feeds.feedburner.com/stefanimhoff/projekte/" />
+    <link rel="alternate" type="application/atom+xml" title="Notizen" href="http://feeds2.feedburner.com/stefanimhoff/notizen/" />
+    <link rel="alternate" type="application/atom+xml" title="Randbemerkungen" href="http://feeds2.feedburner.com/stefanimhoff/randbemerkungen/" />
+    <link rel="alternate" type="application/atom+xml" title="Projekte" href="http://feeds2.feedburner.com/stefanimhoff/projekte/" />
