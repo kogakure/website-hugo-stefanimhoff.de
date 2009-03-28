5f5e1918667cf6d5e2d2c9118680782f5093d468
Stefan Imhoff <stefan@imhoff.name>
2009-03-28 10:22:51 +0100

    Änderung in Python-String

--- fabfile.py
+++ fabfile.py
@@ -7,4 +7,4 @@ config.project_path = '/home/kogakure/webapps/stefanimhoff'
 def deploy():
     "Lokale Änderungen pushen, Änderungen pullen auf server"
     local('git push;', fail='warn')
-    run('cd $(project_path)/; git pull', fail='warn')
+    run('cd %(project_path)s/; git pull', fail='warn')
