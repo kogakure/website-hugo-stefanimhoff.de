a7d6d6a47e1493518048a96cfb8df7298c0a2509
Stefan Imhoff <stefan@imhoff.name>
2009-09-24 20:58:23 +0200

    Werbung Piratenpartei eingebaut

--- templates/main/index.php
+++ templates/main/index.php
@@ -85,6 +85,7 @@
             {if:else}
 
               <div id="aktuelle-arbeiten">
+                <img src="http://stefanimhoff.de/css/images/piratenpartei-transparent.png" alt="Piratenpartei wählen!" title="Wähle Freiheit statt Angst am 27.09.2009" />
                 <h2>Vorgestelltes Projekt</h2>
                 {exp:weblog:entries weblog="projects" limit="1" orderby="date" sort="desc" status="open" rdf="off" disable="member_data|trackbacks"}
                 <a href="{path='projekte'}#pj{entry_id}"><img src="{path='site_index'}images/projekte/{url_title}_thumb.jpg" width="285" height="104" alt="{title}" /></a>
