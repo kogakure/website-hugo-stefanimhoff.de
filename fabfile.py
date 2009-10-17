103f235b60749b16b3785f1c2cf900c964d467ad
Stefan Imhoff <stefan@imhoff.name>
2009-10-17 18:17:38 +0200

    Anpassungen für Fabric 1.0

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-config.fab_hosts = ['kogakure.webfactional.com']
-config.project_path = '/home/kogakure/webapps/stefanimhoff'
+from fabric.api import env, local, run
+
+env.hosts = ['kogakure.webfactional.com']
+env.path = '/home/kogakure/webapps/stefanimhoff'
 
 def deploy():
     "Lokale Änderungen pushen, Änderungen pullen auf server"
-    local('git push;', fail='warn')
-    run('cd %(project_path)s/; git pull', fail='warn')
+    local('git push;')
+    run('cd %(path)s/; git pull' % env, pty=True)
