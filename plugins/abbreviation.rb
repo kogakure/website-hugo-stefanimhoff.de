7bffe7eb4ffa895b15d11dc484fbc9ffc18d6b86
Stefan Imhoff <stefan.imhoff@xing.com>
2012-07-25 17:28:18 +0200

    Made the plugin more modular

--- plugins/abbreviation.rb
+++ plugins/abbreviation.rb
@@ -6,7 +6,7 @@ module Jekyll
       plugin_root = File.dirname(__FILE__)
       abbr_dict = YAML.load(File.open(File.join(plugin_root, "abbreviation.yml")))
       abbr_dict.each do |abbr, title|
-        input.gsub! /\b#{abbr}\b/, "<abbr title=\"#{title}\">#{abbr}</abbr>"
+        input.gsub! /#{abbr}/, "<abbr title=\"#{title}\">#{abbr[/[A-Za-z0-9\.\s\;\&]+/]}</abbr>"
       end
       input
     end
