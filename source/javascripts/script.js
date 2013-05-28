2fe2623bbf0468b8328f432628d76060109f4835
Stefan Imhoff <stefan.imhoff@xing.com>
2013-05-28 10:18:52 +0200

    Fixed problem with Widont plugin

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,6 +1,15 @@
 $(function() {
   // Widon't on all text
-  $(".article p, .article li, .article dd, .article dt, .article h1, .article h2, .article h3, .article h4, .article h5, .article h6").widont();
+  $(".page p,  .post p,  \
+     .page li, .post li, \
+     .page dd, .post dd, \
+     .page dt, .post dt, \
+     .page h1, .post h1, \
+     .page h2, .post h2, \
+     .page h3, .post h3, \
+     .page h4, .post h4, \
+     .page h5, .post h5, \
+     .page h6, .post h6").widont();
 
   // Fitvids
   $(".article").fitVids();
