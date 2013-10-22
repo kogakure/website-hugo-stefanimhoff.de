f645da85787b90178dfc8f868b3ba62b448328e4
Stefan Imhoff <stefan.imhoff@xing.com>
2013-10-22 12:26:08 +0200

    Fixes broken videos

--- source/javascripts/libs/plugins/jquery.fitvids.js
+++ source/javascripts/libs/plugins/jquery.fitvids.js
@@ -54,18 +54,13 @@
 
       var $allVideos = $(this).find(selectors.join(','));
       $allVideos = $allVideos.not("object object"); // SwfObj conflict patch
-      
+
       $allVideos.each(function(){
         var $this = $(this);
         if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
         var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
             width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
             aspectRatio = height / width;
-        
-        console.log("height", height);
-        console.log("width", width);
-        console.log(aspectRatio);
-        
         if(!$this.attr('id')){
           var videoID = 'fitvid' + Math.floor(Math.random()*999999);
           $this.attr('id', videoID);
