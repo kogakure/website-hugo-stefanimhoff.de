7b2ba5c3bad44de9a41a3b703a25688becf43534
Stefan Imhoff <stefan.imhoff@xing.com>
2013-11-28 13:10:04 +0100

    Bouncing animation on off-screen menu

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -4,38 +4,6 @@ var timer,
 
 $(function() {
 
-  // Scroll smoothly to the top of the page
-  // ======================================
-  $("#top-link").on("click", function(event) {
-
-    // debugger;
-    position = $(window).scrollTop(); // Get the scroll position
-
-    // Set the body top margin
-    $("body").css({
-      "margin-top": -position+"px",
-      "overflow-y": "scroll", // This property is posed for fix the blink of the window width change
-    });
-
-    // Make the scroll handle on the position 0
-    $(window).scrollTop(0);
-
-    // Add the transition property to the body element
-    $("body").css("transition", "all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)");
-
-    // Apply the scroll effects
-    $("body").css("margin-top", "0");
-
-    // Wait until the transition end
-    $("body").on("webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd", function() {
-      // Remove the transition property
-      $("body").css("transition", "none");
-    });
-
-    event.preventDefault();
-  });
-
-
   // FastClick
   // =========
   FastClick.attach(document.body);
@@ -53,7 +21,6 @@ $(function() {
 
   // Navigation
   // ==========
-
   // Open navigation by clicking on "open"
   $(".nav-btn").on("click", function(event) {
     $("body").toggleClass("navigation--open");
@@ -91,5 +58,37 @@ $(function() {
   $('#nav a').filter(function() {
       return this.href == url;
   }).addClass('navigation--active');
+
+
+  // Scroll smoothly to the top of the page
+  // ======================================
+  $("#top-link").on("click", function(event) {
+
+    // debugger;
+    position = $(window).scrollTop(); // Get the scroll position
+
+    // Set the body top margin
+    $("body").css({
+      "margin-top": -position+"px",
+      "overflow-y": "scroll", // This property is posed for fix the blink of the window width change
+    });
+
+    // Make the scroll handle on the position 0
+    $(window).scrollTop(0);
+
+    // Add the transition property to the body element
+    $("body").css("transition", "all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)");
+
+    // Apply the scroll effects
+    $("body").css("margin-top", "0");
+
+    // Wait until the transition end
+    $("body").on("webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd", function() {
+      // Remove the transition property
+      $("body").css("transition", "none");
+    });
+
+    event.preventDefault();
+  });
 
 });
