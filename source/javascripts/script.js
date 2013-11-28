18c36d938cc9b9e47b767318750e2a1edbb8994a
Stefan Imhoff <stefan.imhoff@xing.com>
2013-11-28 10:51:34 +0100

    Smooth scrooling for top link (animation curve needs some adjustments)

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -49,8 +49,41 @@ $(function() {
   });
 
   // Hightlight current navigation item
+  // ==================================
   $('#nav a').filter(function() {
       return this.href == url;
   }).addClass('navigation--active');
+
+
+  // Scroll smoothly to the top of the page
+  // ======================================
+  $("#top-link").on("click", function(event){
+
+    event.preventDefault();
+
+    // Get the scroll position
+    position = $(window).scrollTop();
+
+    // Set the body top margin
+    $("body").css({
+      "margin-top": -position+"px",
+      "overflow-y": "scroll", // This property is posed for fix the blink of the window width change
+    });
+
+    // Make the scroll handle on the position 0
+    $(window).scrollTop(0);
+
+    // Add the transition property to the body element
+    $("body").css("transition", "all 1s cubic-bezier(0.175, 0.885, 0.320, 1.275)");
+
+    // Apply the scroll effects
+    $("body").css("margin-top", "0");
+
+    // Wait until the transition end
+    $("body").on("webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd", function(){
+      // Remove the transition property
+      $("body").css("transition", "none");
+    });
+  });
 
 });
