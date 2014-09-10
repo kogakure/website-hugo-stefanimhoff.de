9a82f442ecec6a6d23d67ba5b91bf1644e0803b1
Stefan Imhoff <stefan.imhoff@xing.com>
2014-09-10 15:57:00 +0200

    Add more plugins

---
+++ app/_plugins/ext.rb
@@ -0,0 +1,8 @@
+require "jekyll-assets"
+require "jekyll-assets/compass"
+require "uglifier"
+require 'singularitygs'
+require 'color-schemer'
+require 'toolkit'
+require 'modular-scale'
+require 'breakpoint'
