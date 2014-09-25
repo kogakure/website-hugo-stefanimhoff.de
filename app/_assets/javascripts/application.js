026984b5e152aec86055ecaa10aa0ddb38a45ea7
Stefan Imhoff <stefan.imhoff@xing.com>
2014-09-25 15:08:32 +0200

    Move Navigation open/close to Vanilla JavaScript

--- app/_assets/javascripts/application.js
+++ app/_assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require fastclick
+//= require libs/classie
 //= require libs/ios-rotate-scaling-fix
 //= require libs/fitvids
 //= require main
--- app/_assets/javascripts/main.js
+++ app/_assets/javascripts/main.js
@@ -1,106 +1,109 @@
 /*global $:false, jQuery:false, FastClick:false, ga:false */
 
-var timer,
-    position = $(window).scrollTop(),
-    url = window.location.href;
+if ('querySelector' in document && 'addEventListener' in window) {
+  var timer,
+      position      = $(window).scrollTop(),
+      url           = window.location.href,
+      body          = document.querySelector('body'),
+      container     = document.querySelector('.container'),
+      triggerButton = document.querySelector('.js-nav-btn')
+      closeButton   = document.querySelector('.js-nav-close-btn');
 
-$(function() {
-
-  // FastClick
-  // =========
-  FastClick.attach(document.body);
-
-
-  // Fitvids
-  // =======
-  $('.container').fitVids();
-
-
-  // Navigation
-  // ==========
-  // Open navigation by clicking on 'open'
-  $('.js-nav-btn').on('click', function(event) {
-    $('body').toggleClass('navigation-is-open');
+  /**
+  * Open navigation menu
+  */
+  function toggleNavigation() {
+    classie.toggle(body, 'navigation-is-open');
     event.preventDefault();
     event.stopImmediatePropagation();
-  });
+  }
 
-
-  // Close navigation by clicking on 'close'
-  $('.js-nav-close-btn').on('click', function(event) {
-    $('body').removeClass('navigation-is-open');
-    event.preventDefault();
-    event.stopImmediatePropagation();
-  });
-
-
-  // Close navigation by clicking somewhere in the content
-  // but not on link or if navigation is closed
-  $('.container').on('click', function(event) {
-    if ($('body.navigation-is-open').length > 0) {
-      if(event.target.className !== 'nav-btn') {
-        $('body').removeClass('navigation-is-open');
+  /**
+  * Close navigation menu
+  */
+  function closeNavigation() {
+    if (classie.has(body, 'navigation-is-open')) {
+      if (event.target.className !== 'nav-btn') {
+        classie.remove(body, 'navigation-is-open');
       }
     }
-  });
+  }
 
 
-  // Hightlight current navigation item
-  // ==================================
-  $('#nav a').filter(function() {
-    return this.href === url;
-  }).closest('li').addClass('nav-is-active');
+  $(function() {
+    FastClick.attach(document.body);
+    fitVids('.container');
+
+    // Navigation menu event listeners
+    triggerButton.addEventListener('click', toggleNavigation);
+    closeButton.addEventListener('click', closeNavigation);
+    container.addEventListener('click', closeNavigation);
 
 
-  // Scroll smoothly to the top of the page
-  // ======================================
-  $('#top-link').on('click', function(event) {
-    var position = $(window).scrollTop(); // Get the scroll position
+    // Hightlight current navigation item
+    // ==================================
+    $('#nav a').filter(function() {
+      return this.href === url;
+    }).closest('li').addClass('nav-is-active');
 
-    // Set the body top margin
-    $('body').css({
-      'margin-top': -position+'px',
-      'overflow-y': 'scroll', // This property is posed for fix the blink of the window width change
+
+    // Scroll smoothly to the top of the page
+    // ======================================
+    $('#top-link').on('click', function(event) {
+      var position = $(window).scrollTop(); // Get the scroll position
+
+      // Set the body top margin
+      $('body').css({
+        'margin-top': -position+'px',
+        'overflow-y': 'scroll', // This property is posed for fix the blink of the window width change
+      });
+
+      // Make the scroll handle on the position 0
+      $(window).scrollTop(0);
+
+      // Add the transition property to the body element
+      $('body').css('transition', 'all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)');
+
+      // Apply the scroll effects
+      $('body').css('margin-top', '0');
+
+      // Wait until the transition end
+      $('body').on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
+        // Remove the transition property
+        $('body').css('transition', 'none');
+      });
+
+      event.preventDefault();
     });
 
-    // Make the scroll handle on the position 0
-    $(window).scrollTop(0);
+    // Tracking aller Links
+    // ====================
+    var trackingLinks = document.querySelectorAll('a');
 
-    // Add the transition property to the body element
-    $('body').css('transition', 'all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)');
+    for (var i = 0, len = trackingLinks.length; i < len; i++) {
+      var trackingLink = trackingLinks[i];
 
-    // Apply the scroll effects
-    $('body').css('margin-top', '0');
+      trackingLink.addEventListener('click', function(event) {
+        var url = this.getAttribute('href'),
+            newtab = false;
 
-    // Wait until the transition end
-    $('body').on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
-      // Remove the transition property
-      $('body').css('transition', 'none');
-    });
+        if (event.currentTarget.host !== window.location.host) {
+          ga('send', 'event', 'Ausgehender Link', 'click', event.target.text + ': ' + event.target.href);
 
-    event.preventDefault();
-  });
+          if (event.metaKey || event.ctrlKey || this.target === '_blank') {
+            newtab = true;
+          }
 
-  // Tracking aller Links
-  // ====================
-  $('a').on('click',function(event){
-    var url = $(this).attr('href'),
-        newtab = false;
+          if (!newtab) {
+            event.preventDefault();
+            setTimeout('document.location = "' + url + '"', 100);
+          }
 
-    if (event.currentTarget.host !== window.location.host) {
-      ga('send', 'event', 'Ausgehender Link', 'click', event.target.text + ': ' + event.target.href);
-
-      if (event.metaKey || event.ctrlKey || this.target === '_blank') {
-        newtab = true;
-      }
-
-      if (!newtab) {
-        event.preventDefault();
-        setTimeout('document.location = "' + url + '"', 100);
-      }
-
-    } else {
-      ga('send', 'event', 'Interner Link', 'click', event.target.text + ': ' + event.target.href);
+        } else {
+          ga('send', 'event', 'Interner Link', 'click', event.target.text + ': ' + event.target.href);
+        }
+      });
     }
+
   });
-});
+}
