82813d450662920b16a8c00f6a1889c86a8738fd
Stefan Imhoff <stefan.imhoff@xing.com>
2014-09-25 16:56:36 +0200

    Move all code to Vanilla JavaScript

--- app/_assets/javascripts/main.js
+++ app/_assets/javascripts/main.js
@@ -1,19 +1,32 @@
-/*global $:false, jQuery:false, FastClick:false, ga:false */
+/*global FastClick:false, ga:false, classie:false */
+
+function addMultipleEventListeners(el, s, fn) {
+  var evts = s.split(' ');
+  for (var i=0, iLen=evts.length; i<iLen; i++) {
+    el.addEventListener(evts[i], fn, false);
+  }
+}
 
 if ('querySelector' in document && 'addEventListener' in window) {
-  var timer,
-      position      = $(window).scrollTop(),
-      url           = window.location.href,
-      body          = document.querySelector('body'),
-      container     = document.querySelector('.container'),
-      triggerButton = document.querySelector('.js-nav-btn')
-      closeButton   = document.querySelector('.js-nav-close-btn');
+  var NAVIGATION_OPEN_CLASS    = 'navigation-is-open',
+      NAVIGATION_TRIGGER_CLASS = 'js-nav-btn',
+      NAVIGATION_CLOSE_CLASS   = 'js-nav-close-btn',
+      timer,
+      position                 = window.scrollY,
+      navigationLinks          = '#nav a',
+      url                      = window.location.href,
+      body                     = document.querySelector('body'),
+      container                = document.querySelector('.container'),
+      triggerButton            = document.querySelector('.' + NAVIGATION_TRIGGER_CLASS),
+      closeButton              = document.querySelector('.' + NAVIGATION_CLOSE_CLASS),
+      topLink                  = document.querySelector('#top-link'),
+      trackingLinks            = document.querySelectorAll('a');
 
   /**
   * Open navigation menu
   */
   function toggleNavigation() {
-    classie.toggle(body, 'navigation-is-open');
+    classie.toggle(body, NAVIGATION_OPEN_CLASS);
     event.preventDefault();
     event.stopImmediatePropagation();
   }
@@ -22,9 +35,9 @@ if ('querySelector' in document && 'addEventListener' in window) {
   * Close navigation menu
   */
   function closeNavigation() {
-    if (classie.has(body, 'navigation-is-open')) {
-      if (event.target.className !== 'nav-btn') {
-        classie.remove(body, 'navigation-is-open');
+    if (classie.has(body, NAVIGATION_OPEN_CLASS)) {
+      if (event.target.className !== NAVIGATION_TRIGGER_CLASS) {
+        classie.remove(body, NAVIGATION_OPEN_CLASS);
       }
     }
   }
@@ -33,15 +46,65 @@ if ('querySelector' in document && 'addEventListener' in window) {
   * Hightlight current navigation item
   */
   function currentNavigationItem() {
-    selectedNavigationItem = Array.prototype.filter.call(document.querySelectorAll("#nav a"), function(target) {
+    selectedNavigationItem = Array.prototype.filter.call(document.querySelectorAll(navigationLinks), function(target) {
       return target.href === url
     });
 
     return selectedNavigationItem[0];
   }
 
+  /**
+   * Scroll smoothly to the top of the page
+   */
+  function scrollSmoothlyToTop() {
+    var position = window.scrollY; // Get the scroll position
 
-  $(function() {
+    // Set the body top margin
+    body.style.marginTop = -position+'px';
+    body.style.overflowY = 'scroll';
+
+    // Make the scroll handle on the position 0
+    window.scrollTo(0, 0);
+
+    // Add the transition property to the body element
+    body.style.transition = 'all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)';
+
+    // Apply the scroll effects
+    body.style.marginTop = '0';
+
+    // Wait until the transition end
+    addMultipleEventListeners(body, 'webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
+      body.style.transition = 'none';
+    });
+
+    event.preventDefault();
+  }
+
+  /**
+   * Track all links with Google Analytics
+   */
+  function trackLinksWithGoogleAnalytics() {
+    var url = this.getAttribute('href'),
+        newtab = false;
+
+    if (event.currentTarget.host !== window.location.host) {
+      ga('send', 'event', 'Ausgehender Link', 'click', event.target.text + ': ' + event.target.href);
+
+      if (event.metaKey || event.ctrlKey || this.target === '_blank') {
+        newtab = true;
+      }
+
+      if (!newtab) {
+        event.preventDefault();
+        setTimeout('document.location = "' + url + '"', 100);
+      }
+
+    } else {
+      ga('send', 'event', 'Interner Link', 'click', event.target.text + ': ' + event.target.href);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
     FastClick.attach(document.body);
     fitVids('.container');
 
@@ -51,66 +114,20 @@ if ('querySelector' in document && 'addEventListener' in window) {
     container.addEventListener('click', closeNavigation);
 
     // Hightlighting of navigation item
-    classie.add(currentNavigationItem().parentElement, 'nav-is-active');
-
+    if (currentNavigationItem()) {
+      classie.add(currentNavigationItem().parentElement, 'nav-is-active');
+    }
 
     // Scroll smoothly to the top of the page
-    // ======================================
-    $('#top-link').on('click', function(event) {
-      var position = $(window).scrollTop(); // Get the scroll position
+    topLink.addEventListener('click', scrollSmoothlyToTop);
 
-      // Set the body top margin
-      $('body').css({
-        'margin-top': -position+'px',
-        'overflow-y': 'scroll', // This property is posed for fix the blink of the window width change
-      });
-
-      // Make the scroll handle on the position 0
-      $(window).scrollTop(0);
-
-      // Add the transition property to the body element
-      $('body').css('transition', 'all 1s cubic-bezier(0.135, 0.780, 0.215, 1.080)');
-
-      // Apply the scroll effects
-      $('body').css('margin-top', '0');
-
-      // Wait until the transition end
-      $('body').on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
-        // Remove the transition property
-        $('body').css('transition', 'none');
-      });
-
-      event.preventDefault();
-    });
 
     // Tracking aller Links
-    // ====================
-    var trackingLinks = document.querySelectorAll('a');
 
     for (var i = 0, len = trackingLinks.length; i < len; i++) {
       var trackingLink = trackingLinks[i];
 
-      trackingLink.addEventListener('click', function(event) {
-        var url = this.getAttribute('href'),
-            newtab = false;
-
-        if (event.currentTarget.host !== window.location.host) {
-          ga('send', 'event', 'Ausgehender Link', 'click', event.target.text + ': ' + event.target.href);
-
-          if (event.metaKey || event.ctrlKey || this.target === '_blank') {
-            newtab = true;
-          }
-
-          if (!newtab) {
-            event.preventDefault();
-            setTimeout('document.location = "' + url + '"', 100);
-          }
-
-        } else {
-          ga('send', 'event', 'Interner Link', 'click', event.target.text + ': ' + event.target.href);
-        }
-      });
+      trackingLink.addEventListener('click', trackLinksWithGoogleAnalytics);
     }
-
   });
 }
