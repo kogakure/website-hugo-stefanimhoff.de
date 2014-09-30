c1655249630386b651df7b6e9e610cb24c69ddf9
Stefan Imhoff <stefan@imhoff.name>
2014-09-30 20:25:32 +0200

    Change order of tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var config = require('../config').watch;
 
-gulp.task('watch', function() {
-  gulp.watch(config.jekyll, ['jekyll'])
-  gulp.watch(config.sass, ['sass']);
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch(config.jekyll,  ['jekyll'])
+  gulp.watch(config.sass,    ['sass']);
   gulp.watch(config.scripts, ['scripts']);
 });
