040bafc45530102c0bd78491a75db63f3c9bcc15
Stefan Imhoff <stefan.imhoff@xing.com>
2014-09-30 14:35:55 +0200

    Add a production option for gulp task

--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -1,5 +1,4 @@
 var gulp        = require('gulp');
-// var shell    = require('gulp-shell');
 var cp          = require('child_process');
 var browserSync = require('browser-sync');
 var config      = require('../config').jekyll;
@@ -7,12 +6,17 @@ var config      = require('../config').jekyll;
 /**
  * Build the Jekyll Site
  */
-// gulp.task('jekyll', shell.task([
-//   'bundle exec jekyll build -q --source app --destination build --config _config.yml'
-// ]));
-
 gulp.task('jekyll', function (done) {
   browserSync.notify('<span style="color: grey">Running:</span> $ jekyll build');
   return cp.spawn('bundle', ['exec', 'jekyll', 'build', '-q', '--source=' + config.src, '--destination=' + config.dest, '--config=' + config.config], {stdio: 'inherit'})
   .on('close', done);
 });
+
+/**
+ * Alternative @TODO: Remove later
+ */
+// var shell    = require('gulp-shell');
+
+// gulp.task('jekyll', shell.task([
+//   'bundle exec jekyll build -q --source app --destination build --config _config.yml'
+// ]));
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,15 +3,28 @@ var sass    = require('gulp-ruby-sass'); // @TODO: Try RubyLib
 var plumber = require('gulp-plumber');
 var config  = require('../config').sass;
 
+var env = process.env.NODE_ENV || 'development'; // NODE_ENV=production gulp sass
+
+/**
+ * Generate CSS from SCSS
+ * Build sourcemaps
+ */
 gulp.task('sass', function() {
+  var sassConfig = {
+    noCache: true,
+    compass: true,
+    bundleExec: true,
+    sourcemap: true,
+    sourcemapPath: '../scss'
+  };
+
+  if (env === 'production') {
+    sassConfig.style = 'compressed';
+    sassConfig.sourcemap = false;
+  }
+
   return gulp.src(config.src)
     .pipe(plumber())
-    .pipe(sass({
-      noCache: true,
-      compass: true,
-      bundleExec: true,
-      sourcemap: true,
-      sourcemapPath: '../scss'
-    }))
+    .pipe(sass(sassConfig))
     .pipe(gulp.dest(config.dest));
 });
