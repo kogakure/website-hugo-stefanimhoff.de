22503d9254ef9d0a31b841863e16e9e699361164
Stefan Imhoff <stefan@imhoff.name>
2014-10-04 22:03:46 +0200

    Add empty task for publishing

--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -6,7 +6,7 @@ var config      = require('../config').jekyll;
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll', function (done) {
+gulp.task('jekyll', function(done) {
   browserSync.notify('Compiling Jekyll');
 
   return cp.spawn('bundle', ['exec', 'jekyll', 'build', '-q', '--source=' + config.src, '--destination=' + config.dest, '--config=' + config.config], { stdio: 'inherit' })
---
+++ gulp/tasks/publish.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+
+gulp.task('publish', function() {
+  console.log('@TODO: Minimize, optimize and sync');
+});
