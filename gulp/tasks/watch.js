80962dc32c09d2b31d27284badfe02c1ac9f815d
Stefan Imhoff <stefan@imhoff.name>
2014-10-05 14:39:22 +0200

    Auto check scss and javascript on save

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,8 +3,8 @@ var config = require('../config');
 
 gulp.task('watch', ['browserSync'], function() {
   gulp.watch(config.watch.jekyll,  ['jekyll'])
-  gulp.watch(config.watch.sass,    ['sass']);
-  gulp.watch(config.watch.scripts, ['scripts']);
+  gulp.watch(config.watch.sass,    ['sass', 'scsslint']);
+  gulp.watch(config.watch.scripts, ['scripts', 'jshint']);
   gulp.watch(config.watch.images,  ['images']);
   gulp.watch(config.watch.svg,     ['copyFonts']);
 });
