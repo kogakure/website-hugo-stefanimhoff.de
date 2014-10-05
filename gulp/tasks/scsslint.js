27fdb565c69440247a8fcb70ebeb591ac5830931
Stefan Imhoff <stefan@imhoff.name>
2014-10-05 13:54:34 +0200

    Formatting and variables for SCSS lint task

--- gulp/tasks/scsslint.js
+++ gulp/tasks/scsslint.js
@@ -1,7 +1,8 @@
-var gulp = require('gulp');
+var gulp     = require('gulp');
 var scsslint = require('gulp-scss-lint');
+var config   = require('../config');
 
 gulp.task('scsslint', function() {
-  gulp.src('app/_assets/scss/**/*.scss')
+  gulp.src(config.sass.src)
     .pipe(scsslint());
 });
