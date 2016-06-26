1a05511cba28e13291218fac1fef98e477707508
Stefan Imhoff <stefan@imhoff.name>
2016-06-26 08:58:22 +0200

    Whitespace removal

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -2,7 +2,7 @@ var gulp        = require('gulp');
 var runSequence = require('run-sequence');
 
 /**
- * 
+ *
  */
 gulp.task('publish', function(callback) {
   runSequence('production:build', 'rsync', 'open-browser', callback);
