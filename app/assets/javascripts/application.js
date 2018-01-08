abaeed9ab4c0e2524cdc101aef3d1014f3551362
Stefan Imhoff <stefan@imhoff.name>
2018-01-08 16:34:42 +0100

    chore: Remove fastclick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@ require('./vendor/ios-rotate-scaling-fix');
 
 var classie = require('classie');
 var fitvids = require('fitvids');
-var attachFastClick = require('fastclick');
 var navigation = require('./libs/navigation');
 var tracking = require('./libs/tracking');
 
@@ -10,22 +9,25 @@ var trackingLinks = document.querySelectorAll('a');
 
 if ('querySelector' in document && 'addEventListener' in window) {
   document.addEventListener('DOMContentLoaded', function() {
-    // FastClick
-    attachFastClick(document.body);
-
     // Fitvids
     fitvids('.container');
 
     // Hightlighting of navigation item
     if (navigation.currentNavigationItem()) {
-      classie.add(navigation.currentNavigationItem().parentElement, 'nav-is-active');
+      classie.add(
+        navigation.currentNavigationItem().parentElement,
+        'nav-is-active'
+      );
     }
 
     // Tracking of all links
     for (var i = 0, len = trackingLinks.length; i < len; i++) {
       var trackingLink = trackingLinks[i];
 
-      trackingLink.addEventListener('click', tracking.trackLinksWithGoogleAnalytics);
+      trackingLink.addEventListener(
+        'click',
+        tracking.trackLinksWithGoogleAnalytics
+      );
     }
   });
 }
