7950c171772af5505f8ee8f1ee05611d5bef68cd
Stefan Imhoff <stefan@imhoff.name>
2018-01-26 20:39:14 +0100

    test: Add linting for CSS and JavaScript as npm task

--- app/assets/javascripts/libs/navigation.js
+++ app/assets/javascripts/libs/navigation.js
@@ -1,17 +1,17 @@
-/* global classie:false */
-'use strict';
-
-var navigationLinks = '.nav a',
-    url             = window.location.href;
+var navigationLinks = '.nav a';
+var url = window.location.href;
 
 module.exports = {
   /**
    * Hightlight current navigation item
    */
   currentNavigationItem: function() {
-    var selectedNavigationItem = Array.prototype.filter.call(document.querySelectorAll(navigationLinks), function(target) {
-      return target.href === url;
-    });
+    var selectedNavigationItem = Array.prototype.filter.call(
+      document.querySelectorAll(navigationLinks),
+      function(target) {
+        return target.href === url;
+      }
+    );
 
     return selectedNavigationItem[0];
   }
--- app/assets/javascripts/libs/tracking.js
+++ app/assets/javascripts/libs/tracking.js
@@ -1,15 +1,18 @@
 /* global ga:false */
-'use strict';
-
-var url = window.location.href;
-
+/* eslint no-implied-eval: "off" */
 module.exports = {
   trackLinksWithGoogleAnalytics: function() {
     var url = this.getAttribute('href'),
-        newtab = false;
+      newtab = false;
 
     if (event.currentTarget.host !== window.location.host) {
-      ga('send', 'event', 'Ausgehender Link', 'click', event.target.text + ': ' + event.target.href);
+      ga(
+        'send',
+        'event',
+        'Ausgehender Link',
+        'click',
+        event.target.text + ': ' + event.target.href
+      );
 
       if (event.metaKey || event.ctrlKey || this.target === '_blank') {
         newtab = true;
@@ -19,9 +22,14 @@ module.exports = {
         event.preventDefault();
         setTimeout('document.location = "' + url + '"', 100);
       }
-
     } else {
-      ga('send', 'event', 'Interner Link', 'click', event.target.text + ': ' + event.target.href);
+      ga(
+        'send',
+        'event',
+        'Interner Link',
+        'click',
+        event.target.text + ': ' + event.target.href
+      );
     }
   }
 };
